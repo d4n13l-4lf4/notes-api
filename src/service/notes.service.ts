45cdc69ad629bbe151cf920174db5a35c93ad4d8
d4n13l-4lf4 <d.aucatoma96@gmail.com>
2020-07-28 22:20:22 -0500

    Adding test data to notes service just for testing

--- src/service/notes.service.ts
+++ src/service/notes.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Note } from '../models/Note';
 
 @Injectable()
 export class NotesService {
 
-  findAll() {
-    return [];
+  data: Array<Note>;
+
+  constructor() {
+    this.data = [
+      {id: 1, description: 'Mi nota 1'},
+      {id: 2, description: 'Mi nota 2'}
+    ];
   }
 
-  findById(id: string) {
-    return {id: 1, description: '2'};
+  findAll(): Array<Note> {
+    return this.data;
+  }
+
+  findById(id: string): Note {
+    return this.data.filter(n => n.id.toString() === id)[0];
   }
 
 
